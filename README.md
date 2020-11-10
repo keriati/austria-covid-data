a41af107f2c9b20636ffea04bb1e39dfc264bfcd
Attila Kerekes <attila.kerekes@erstegroup.com>
2020-11-10 18:48:12 +0100

    Update sync

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Austria COVID Data Archive Repository
 
+## Update 2020.11.10
+
+As the format from https://covid19-dashboard.ages.at/data/data.zip changed
+the data source from https://info.gesundheitsministerium.at/data/data.zip was added back.
+
+## Update 2020.10.6 
+
 This repository is providing a daily archive from the data set provided by
 
     https://covid19-dashboard.ages.at/
@@ -10,7 +17,9 @@ The data is pulled every hour from
     
 extracted and committed.
 
-Before *Tue Oct 6 19:03:57 2020 +0200* the data was provided by
+## Before *Tue Oct 6 19:03:57 2020 +0200* 
+
+This repository is providing a daily archive from the data set provided by
 
     https://info.gesundheitsministerium.at/
 
--- sync.sh
+++ sync.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-readonly DATA_URL_AEGIS="https://covid19-dashboard.ages.at/data/data.zip"
+readonly DATA_URL_AGES="https://covid19-dashboard.ages.at/data/data.zip"
 readonly DATA_URL="https://info.gesundheitsministerium.at/data/data.zip"
+readonly DATA_ARCHIVE_NAME_AGES="data-ages.zip"
 readonly DATA_ARCHIVE_NAME="data.zip"
 readonly DATE_STAMP="$(date +"%F-%H%M%S")"
+readonly DATA_PATH_AGES="data/data-ages-$DATE_STAMP"
 readonly DATA_PATH="data/data-$DATE_STAMP"
 readonly COMMIT_MESSAGE="Sync $DATE_STAMP"
 
+curl $DATA_URL_AGES --output $DATA_ARCHIVE_NAME_AGES --silent
 curl $DATA_URL --output $DATA_ARCHIVE_NAME --silent
 
 if test -f "$DATA_ARCHIVE_NAME"; then
+    unzip -qq $DATA_ARCHIVE_NAME_AGES -d $DATA_PATH_AGES
     unzip -qq $DATA_ARCHIVE_NAME -d $DATA_PATH
+    rm $DATA_ARCHIVE_NAME_AGES
     rm $DATA_ARCHIVE_NAME
     git add --all >/dev/null
     git commit -am "$COMMIT_MESSAGE" --quiet
